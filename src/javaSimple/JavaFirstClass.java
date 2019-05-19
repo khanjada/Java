220d81cb320af094b6aea2674d063997eef774fd
hurayra <khanjada.hurayra.qa@gmail.com>
2019-05-19 01:13:54 -0400

    java beginner First commit

---
+++ src/javaSimple/JavaFirstClass.java
@@ -0,0 +1,16 @@
+package javaSimple;
+
+public class JavaFirstClass {
+
+
+
+    public static void  main(String []args){
+
+System.out.println("LIFE IS PRECIOUS");
+System.out.println("LIFE IS BEAUTIFUL");
+System.out.println("LIFE IS PRECIOUS");
+
+    }
+
+
+}
